c9e4dd91cce4a331f1e7434bd809db20735760cf
GIORGIO CERUTTI <giorgio.cerutti@dune.it>
2024-02-12 16:12:21 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './components/home/home.component';
+import { MapComponent } from './components/map/map.component';
+import {MatToolbarModule} from '@angular/material/toolbar'
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import { MatButtonModule} from '@angular/material/button';
+import { GoogleMapsModule } from '@angular/google-maps';
+import { NavSideBarComponent } from './components/nav-side-bar/nav-side-bar.component';
+import { GeoLocationService } from './services/geo-location.service';
 
+const Material_Imports = [
+  MatToolbarModule,
+  MatIconModule,
+  MatSidenavModule,
+  MatListModule,
+  MatButtonModule,
+  GoogleMapsModule
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MapComponent,
+    NavSideBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    Material_Imports
   ],
-  providers: [],
+  providers: [GeoLocationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
